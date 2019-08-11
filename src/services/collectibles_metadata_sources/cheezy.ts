cd6e01fdc04bec0ac16c08efa1a0d6850fb2e3e3
Igor Line <linesethenoiz@gmail.com>
2019-08-11 13:48:17 +0200

    Add cheezy metadata gateway

---
+++ src/services/collectibles_metadata_sources/cheezy.ts
@@ -0,0 +1,89 @@
+import { ERC721TokenEvents } from '@0x/contract-wrappers';
+import { RateLimit } from 'async-sema';
+
+import { COLLECTIBLE_ADDRESS, NETWORK_ID, OPENSEA_API_KEY } from '../../common/constants';
+import { getContractWrappers } from '../../services/contract_wrappers';
+import { getWeb3Wrapper } from '../../services/web3_wrapper';
+import { Collectible, CollectibleMetadataSource } from '../../util/types';
+
+export class Cheezy implements CollectibleMetadataSource {
+    private readonly _rateLimit: () => Promise<void>;
+
+    private readonly _endpointsUrls: { [key: number]: string } = {
+        1: 'https://api.opensea.io/api/v1',
+        4: 'https://rinkeby-api.opensea.io/api/v1',
+        // Add endpoint for development network with ID 50 (default in 0x-project-launch-kit)
+        50: 'https://api.opensea.io/api/v1',
+    };
+
+    public static getAssetsAsCollectible(assets: any[], userAddress?: string): Collectible[] {
+        return assets.map((asset: any) => {
+            return Cheezy.getAssetAsCollectible(asset, userAddress);
+        });
+    }
+
+    public static getAssetAsCollectible(asset: any, userAddress?: string): Collectible {
+        return {
+            tokenId: asset.token_id,
+            name: asset.name || `${asset.asset_contract.name} - #${asset.token_id}`,
+            color: asset.background_color ? `#${asset.background_color}` : '',
+            image: asset.image_url,
+            // Substituting owner for current user address
+            currentOwner: userAddress ? userAddress : asset.owner.address,
+            assetUrl: asset.external_link,
+            description: asset.name,
+            order: null,
+        };
+    }
+
+    constructor(options: { rps: number }) {
+        this._rateLimit = RateLimit(options.rps); // requests per second
+    }
+
+    public async fetchAllUserCollectiblesAsync(userAddress: string): Promise<Collectible[]> {
+        const metadataSourceUrl = this._endpointsUrls[NETWORK_ID];
+        // changed contract address for mainnet cheeze wizards
+        const contractAddress = '0x2f4bdafb22bd92aa7b7552d270376de8edccbc1e';
+
+        // getting transfer events for local development
+        const contractWrappers = await getContractWrappers();
+        const web3Wrapper = await getWeb3Wrapper();
+        const latestBlock = await web3Wrapper.getBlockNumberAsync();
+        const events = await contractWrappers.erc721Token.getLogsAsync(
+            COLLECTIBLE_ADDRESS,
+            ERC721TokenEvents.Transfer,
+            {
+                fromBlock: 0,
+                toBlock: latestBlock,
+            },
+            { _to: userAddress, _tokenId: '1' },
+        );
+        // parsing ids from Transfer events
+        const tokenIds = events.map(e => e.args._tokenId.toString());
+
+        // fetching assets by its ids
+        const tokenIdsQueryParam = tokenIds.map((id: string) => `token_ids=${id}`).join('&');
+        const url = `${metadataSourceUrl}/assets?asset_contract_address=${contractAddress}&${tokenIdsQueryParam}`;
+        const assetsResponse = await this._fetch(url);
+        const assetsResponseJson = await assetsResponse.json();
+        return Cheezy.getAssetsAsCollectible(assetsResponseJson.assets, userAddress);
+    }
+
+    public async fetchCollectiblesAsync(tokenIds: string[]): Promise<Collectible[]> {
+        const metadataSourceUrl = this._endpointsUrls[NETWORK_ID];
+        // changed contract address for mainnet cheeze wizards
+        const contractAddress = '0x2f4bdafb22bd92aa7b7552d270376de8edccbc1e';
+        const tokenIdsQueryParam = tokenIds.map((id: string) => `token_ids=${id}`).join('&');
+        const url = `${metadataSourceUrl}/assets?asset_contract_address=${contractAddress}&${tokenIdsQueryParam}`;
+        const assetsResponse = await this._fetch(url);
+        const assetsResponseJson = await assetsResponse.json();
+        return Cheezy.getAssetsAsCollectible(assetsResponseJson.assets);
+    }
+
+    private readonly _fetch = async (url: string) => {
+        await this._rateLimit();
+        return fetch(url, {
+            headers: { 'X-API-KEY': OPENSEA_API_KEY || '' } as any,
+        });
+    };
+}
--- src/services/collectibles_metadata_sources/index.ts
+++ src/services/collectibles_metadata_sources/index.ts
@@ -1,12 +1,14 @@
 import { COLLECTIBLES_SOURCE } from '../../common/constants';
 import { CollectibleMetadataSource } from '../../util/types';
 
+import { Cheezy } from './cheezy';
 import { Mocked } from './mocked';
 import { Opensea } from './opensea';
 
 const sources: { [key: string]: CollectibleMetadataSource } = {
     opensea: new Opensea({ rps: 5 }),
     mocked: new Mocked(),
+    cheezy: new Cheezy({ rps: 5 }),
 };
 
 export const getConfiguredSource = () => {
